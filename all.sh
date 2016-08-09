d465bf2e4e235ed7b7168e0d85a5d435bf38b4a9
mbiundo <mbiundo@users.noreply.github.com>
2016-08-09 16:47:29 -0700

    updated to all.sh

--- all.sh
+++ all.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-./vgraph.sh
-./igraph.sh
-./tgraph.sh
-./cgraph.sh
-./vbatt-lvd_graph.sh
 ./CommDurationgraph.sh
+./MARC_vgraph.sh
+./MARC_igraph.sh
+./MARC_tgraph.sh
+./MARC_cgraph.sh
+./MARC_vbatt-lvd_graph.sh
 
-eog voltage_graph.png
+./LCCR_vgraph.sh
+./LCCR_igraph.sh
+./LCCR_tgraph.sh
+./LCCR_cgraph.sh
+./LCCR_vbatt-lvd_graph.sh
+
+
+#eog MARC_voltage_graph.png
